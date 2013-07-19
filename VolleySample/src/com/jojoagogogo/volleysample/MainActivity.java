6572e11175343df9707dc34ae598ed5c75dbb5e8
hiroshi.kitano <jojoagogogo@gmail.com        name = kitanp>
2013-07-19 10:06:14 +0900

    add loading dialog

--- VolleySample/src/com/jojoagogogo/volleysample/MainActivity.java
+++ VolleySample/src/com/jojoagogogo/volleysample/MainActivity.java
@@ -3,6 +3,7 @@ package com.jojoagogogo.volleysample;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,38 +20,55 @@ import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 
-	public void requestJson(final Context context) {
-		RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-		String url = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=android";
-		queue.add(new JsonObjectRequest(
-				Method.GET,
-				url,
-				null,
-				new Listener<JSONObject>() {
-					public void onResponse(JSONObject jsonRoot) {
-						Gson gson = new Gson();
-						gson.toJson(jsonRoot);
-						Toast.makeText(context,gson.toJson(jsonRoot), Toast.LENGTH_LONG).show();
-					}
-				}, null));
-		queue.start();
-	}
+    static ProgressDialog dialog = null;
+    public void requestJson(final Context context) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=android";
+        queue.add(new JsonObjectRequest(
+                Method.GET,
+                url,
+                null,
+                new Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonRoot) {
+                        if (dialog != null){
+                            dialog.dismiss();
+                        }
+                        Gson gson = new Gson();
+                        gson.toJson(jsonRoot);
+                        Toast.makeText(context,gson.toJson(jsonRoot), Toast.LENGTH_LONG).show();
+                    }
+                }, null));
+        dialog = loadingDialog("", "now loading ... ");
+        queue.start();
+    }
 
-	public void onClickRequestJson(View view){
-		this.requestJson(this.getApplicationContext());
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
+    public void onClickRequestJson(View view){
+        this.requestJson(this);
+    }
+
+    public ProgressDialog loadingDialog(String title, String message) {
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setIndeterminate(true);
+        dialog.setTitle(title);
+        dialog.setMessage(message);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.show();
+
+        return dialog;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
